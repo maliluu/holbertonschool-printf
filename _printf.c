048eca9da192d3f35325c83db1ea49a51b5eec07
mal <159127791+maliluu@users.noreply.github.com>
2024-03-30 15:15:33 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -23,7 +23,8 @@ int _printf(const char *format, ...)
 		{
 			if (format[i + 1])
 			{
-				if (format[i + 1] != 'c' && format[i + 1] != 's' && format[i + 1] != '%' && format[i + 1] != 'd' && format[i + 1] != 'i')
+				if (format[i + 1] != 'c' && format[i + 1] != 's'
+				&& format[i + 1] != '%' && format[i + 1] != 'd' && format[i + 1] != 'i')
 				{
 					j += _putchar(format[i]);
 					j += _putchar(format[i + 1]);
